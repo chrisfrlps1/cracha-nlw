87019b1548a83bc0a60a548165fb9168b7cb2888
christian felippe <90785056+chrisfrlps1@users.noreply.github.com>
2021-10-22 00:44:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const linksSocialMedia = {
+  github: 'chrisfrlps1',
+  youtube: 'channel/UC2ytqbU93FLQBELHC4RPe_Q',
+  facebook: 'christian.felippe.1238',
+  instagram: 'felippe_christian',
+  twitter: 'Felps_chris7'
+}
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
